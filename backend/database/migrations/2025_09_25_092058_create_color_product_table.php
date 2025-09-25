c994d9e1606690d62de0c3d96824de8ed3b46798
Nehad <35563834+81joker@users.noreply.github.com>
2025-09-25 11:24:42 +0200

    create the database with tables

--- backend/database/migrations/2025_09_25_092058_create_color_product_table.php
+++ backend/database/migrations/2025_09_25_092058_create_color_product_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::create('color_product', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('color_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('color_product');
     }
 };
