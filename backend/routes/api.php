3a3f16b2e82caa43f4be7fa71d91328fc2346f4b
Tim <tim@Tims-MacBook-Air.local>
2025-10-05 06:59:00 +0200

    backend API

---
+++ backend/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProductController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+//products routes
+Route::get('products',[ProductController::class,"index"]);
+Route::get('products/{category}/category',[ProductController::class,"filterProductsByCategory"]);
+Route::get('products/{brand}/brand',[ProductController::class,"filterProductsByBrand"]);
+Route::get('products/{color}/color',[ProductController::class,"filterProductsByColor"]);
+Route::get('products/{size}/size',[ProductController::class,"filterProductsBySize"]);
+Route::get('products/{searchTerm}/find',[ProductController::class,"findProductsByTerm"]);
+Route::get('products/{product}/show',[ProductController::class,"show"]);
