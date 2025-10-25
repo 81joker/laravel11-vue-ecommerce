72341549621ae35354e4e5464bf1155ad557b1c7
Nehad <35563834+81joker@users.noreply.github.com>
2025-10-25 15:06:17 +0200

    redirect the cart footer to logi if is not login

--- frontend/src/components/products/ProductsListItem.vue
+++ frontend/src/components/products/ProductsListItem.vue
@@ -1,14 +1,12 @@
 <template>
     <div class="col-md-6">
         <div class="card mb-2" style="max-width: 320px">
-            <img :src="product.thumbnail" class="card-img-top" 
-                :alt="product.name" height="279">
+            <img :src="product.thumbnail" class="card-img-top" :alt="product.name" height="279">
             <div class="card-body">
-                <router-link class="text-decoration-none text-dark"
-                    :to="`/product/${product.slug}`">
+                <router-link class="text-decoration-none text-dark" :to="`/product/${product.slug}`">
                     <h5 class="card-title">{{ product.name }}</h5>
                 </router-link>
-                <p class="card-text" v-dompurify-html="product.desc.substr(0,50)"></p>
+                <p class="card-text" v-dompurify-html="product.desc.substr(0, 50)"></p>
                 <div class="d-inline-flex mb-2">
                     <span class="badge text-bg-light">
                         <i class="bi bi-tag"></i>
@@ -28,61 +26,78 @@
                     </span>
                 </div>
                 <div v-if="product.reviews.length > 0">
-                    <StarRating
-                        v-model:rating="reviewAvg"
-                        :show-rating="false"
-                        read-only
-                        :star-size="24"
-                    />
+                    <StarRating v-model:rating="reviewAvg" :show-rating="false" read-only :star-size="24" />
                     <small class="text-muted">
                         {{ product.reviews.length }}
-                        {{ product.reviews.length > 1 ? "reviews" : "review"}}
+                        {{ product.reviews.length > 1 ? "reviews" : "review" }}
                     </small>
                 </div>
             </div>
 
-            <!--TODO: condition for showing the button after the login and to do route to login page-->
             <div class="card-footer d-flex justify-content-between bg-light">
-                <router-link :to="`/product/${product.slug}`" class="btn btn-danger btn-sm">
-                   <i class="bi bi-cart-plus"></i> Add to Cart
-                </router-link>
-                <button class="btn btn-outline-danger btn-sm"
-                    @click="favoritesStore.addToFavorites(product)"
-                    v-if="!favoritesStore.checkIfProductAlreadyAddedToFavorites(product)"
-                >
-                    <i class="bi bi-heart"></i>
+                <button class="btn btn-danger btn-sm" @click="onAddToCartClick">
+                    <i class="bi bi-cart-plus"></i> Add to Cart
                 </button>
-                <button class="btn btn-outline-danger btn-sm"
-                    @click="favoritesStore.addToFavorites(product)"
-                    v-else
-                >
-                    <i class="bi bi-heart-fill"></i>
-                </button>
-            </div>
-        </div>
+                <div v-if="authStore.isLoggedIn">
+                    <button class="btn btn-outline-danger btn-sm" @click="favoritesStore.addToFavorites(product)"
+                        v-if="!favoritesStore.checkIfProductAlreadyAddedToFavorites(product)">
+                        <i class="bi bi-heart"></i>
+                    </button>
+                    <button class="btn btn-outline-danger btn-sm" @click="favoritesStore.addToFavorites(product)"
+                        v-else>
+                        <i class="bi bi-heart-fill"></i>
+                    </button>
+                </div>
+                <div v-else>
+                    <button class="btn btn-outline-danger btn-sm" @click="onFavClick">
+                        <i class="bi bi-heart"></i>
+                    </button>
+                </div>
 
+            </div>
+
+
+        </div>
     </div>
 </template>
 
 <script setup>
-    import { computed } from "vue"
-    import StarRating from "vue-star-rating"
-    import { useFavoritesStore } from "../../stores/useFavoritesStore"
+import { computed } from "vue"
+import StarRating from "vue-star-rating"
+import { useFavoritesStore } from "../../stores/useFavoritesStore"
+import { useRouter } from "vue-router"
+import { useAuthStore } from "@/stores/useAuthStore"
 
-    //define the store
-    const favoritesStore = useFavoritesStore()
+//define the store
+const router = useRouter()
+const favoritesStore = useFavoritesStore()
+const authStore = useAuthStore()
 
-    //define the props
-    const props = defineProps({
-        product: {
-            type: Object,
-            required: true
-        }
-    })
+const onAddToCartClick = () => {
+    if (authStore.isLoggedIn) {
 
-    //calculate the average reviews of the product
-    const reviewAvg = computed(() => props.product.reviews.reduce((acc,review) => acc + review.rating / props.product.reviews.length,0))
+        router.push(`/product/${props.product.slug}`)
+    } else {
+        router.push({ name: 'login' })
+    }
+}
+const onFavClick = () => {
+    if (!authStore.isLoggedIn) {
+        router.push({ name: 'login' })
+        return
+    }
+}
+
+//define the props
+const props = defineProps({
+    product: {
+        type: Object,
+        required: true
+    }
+})
+
+//calculate the average reviews of the product
+const reviewAvg = computed(() => props.product.reviews.reduce((acc, review) => acc + review.rating / props.product.reviews.length, 0))
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
