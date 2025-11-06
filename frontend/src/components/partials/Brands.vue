8bb7c5f75979f84c2a0ac62ca5f101059e9ca30f
Nehad <35563834+81joker@users.noreply.github.com>
2025-11-06 10:50:17 +0100

    make the bug in navbar and collapes in sidebar

--- frontend/src/components/partials/Brands.vue
+++ frontend/src/components/partials/Brands.vue
@@ -1,31 +1,41 @@
 <template>
   <div class="mb-3">
-          <h6 id="accordion-collapse-heading-brands">
-        <button type="button" data-accordion-target="#accordion-collapse-brand" aria-expanded="true"
-          aria-controls="accordion-collapse-brand">
-          Brands
-        </button>
-      </h6>
-            <div id="accordion-collapse-brand" aria-labelledby="accordion-collapse-heading-brands">
-    <ul class="nav d-flex flex-column">
-        <li class="nav-item mb-1" v-for="brand in productsStore.brands"
-          :key="brand.id">
-            <button class="btn btn-sm btn-link text-dark"
-              @click="productsStore.filterProducts('brand',brand.slug)"
-              >
-                {{ brand.name }}
-            </button>
+    <h6 id="accordion-collapse-heading-brands">
+      <a
+        class="text-decoration-none link-dark link-opacity-50-hover"
+        data-bs-toggle="collapse"
+        href="#collapseBrands"
+        role="button"
+        aria-expanded="false"
+        aria-controls="collapseBrands"
+      >
+        Brands
+      </a>
+    </h6>
+    <div id="collapseBrands">
+      <ul class="nav d-flex flex-column">
+        <li
+          class="nav-item mb-1"
+          v-for="brand in productsStore.brands"
+          :key="brand.id"
+        >
+          <button
+            class="btn btn-sm btn-link text-dark"
+            @click="productsStore.filterProducts('brand', brand.slug)"
+          >
+            {{ brand.name }}
+          </button>
         </li>
-    </ul>
-            </div>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup>
-  import { useProductStore } from "../../stores/useProductStore";
+import { useProductStore } from "../../stores/useProductStore";
 
-  //define the store variable
-  const productsStore = useProductStore()
+//define the store variable
+const productsStore = useProductStore();
 </script>
 
 <style scoped>
--- frontend/src/components/partials/Categories.vue
+++ frontend/src/components/partials/Categories.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="mb-3">
-      <h6 id="accordion-collapse-heading-category">
-        <button type="button" data-accordion-target="#accordion-collapse-body-category" aria-expanded="true"
-          aria-controls="accordion-collapse-body-category">
-          Category
-        </button>
-      </h6>
-      <div id="accordion-collapse-body-category"  aria-labelledby="accordion-collapse-heading-category">
-        <ul class="nav d-flex flex-column ">
-          <li class="nav-item mb-1" v-for="category in productsStore.categories" :key="category.id">
-            <button class="btn btn-sm btn-link text-dark"
-              @click="productsStore.filterProducts('category', category.slug)">
-              {{ category.name }}
-            </button>
-          </li>
-        </ul>
-      </div>
+    <h6 id="accordion-collapse-heading-category">
+      <a class="text-decoration-none link-dark link-opacity-50-hover" data-bs-toggle="collapse" href="#collapseCategory"
+        role="button" aria-expanded="false" aria-controls="collapseCategory">
+        Category
+      </a>
+    </h6>
+    <div id="collapseCategory">
+      <ul class="nav d-flex flex-column ">
+        <li class="nav-item mb-1" v-for="category in productsStore.categories" :key="category.id">
+          <button class="btn btn-sm btn-link text-dark"
+            @click="productsStore.filterProducts('category', category.slug)">
+            {{ category.name }}
+          </button>
+        </li>
+      </ul>
+    </div>
 
   </div>
 </template>
